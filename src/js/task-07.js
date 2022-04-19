80e2335d78f7b2521cd6b9fd35c5d79d53ec4a5c
zaleskiy88 <78390754+zaleskiy88@users.noreply.github.com>
2022-04-19 20:05:32 +0100

    Delete src directory
    folder src removed

--- src/js/task-07.js
+++
@@ -1,8 +0,0 @@
-const refs = {
-  fontSizeController: document.querySelector("#font-size-control"),
-  text: document.querySelector("#text"),
-};
-
-refs.fontSizeController.addEventListener("input", (event) => {
-  text.style.fontSize = `${event.currentTarget.value}px`;
-});
